26b017b54a41b5562e319aa7815f7f2b95c419c4
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-07-21 23:08:04 +0200

    Add utility functions

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+pretty_stop <- function(...) stop (prettify_text(...))
+
+pretty_message <- function(...) message (prettify_text(...))
+
+pretty_warning <- function(...) warning (prettify_text(...))
+
+prettify_text <- function(...) strwrap(paste0(...))
+    
