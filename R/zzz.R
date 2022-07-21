2c1b0e9c0d00c898784104e53a272279c4016908
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-07-21 23:11:40 +0200

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,11 @@
     if (interactive()) 
         packageStartupMessage(paste0(
 "                           ____  
- _ __   ___   __ _  ___ ___|  _ \ 
-| '_ \ / _ \ / _` |/ __/ __| |_) |
-| | | | (_) | (_| | (__\__ \  _ < 
-|_| |_|\___/ \__,_|\___|___/_| \_\  version ", packageVersion("noacsr"), "\n\n",
+ _ __   ___   __ _  ___ ___|  _ \\ 
+| '_ \\ / _ \\ / _` |/ __/ __| |_) |
+| | | | (_) | (_| | (__\\__ \\  _ < 
+|_| |_|\\___/ \\__,_|\\___|___/_| \\_\\  version ", packageVersion("noacsr"), "\n\n",
+"A Collection of Utility Functions for the NOACS Research Lab \n\n",
 "Use 'create()' to get started and create a new project."
 
         ))
