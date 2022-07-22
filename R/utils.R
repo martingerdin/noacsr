a89a1b5710a44fac154975ce4bc4368de5f6833d
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-07-22 07:32:11 +0200

    Add emoji to pretty messages

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,19 @@
-pretty_stop <- function(...) stop (prettify_text(emoji::emoji("stop_sign"), " ", ...), call. = FALSE)
+pretty_stop <- function(...) stop (prettify_text(emoji = "stop_sign", ...), call. = FALSE)
 
 pretty_message <- function(...) message (prettify_text(...))
 
 pretty_warning <- function(...) warning (prettify_text(...))
 
-prettify_text <- function(...) strwrap(paste0(...), prefix = "\n")
+prettify_text <- function(emoji = NULL, ...) {
+    strwrap(paste0(ifelse(is.null(emoji), "", emoji::emoji(emoji)),
+                   ifelse(is.null(emoji), "", " "),
+                   ...),
+            prefix = "\n")   
+}
+
+ask_question <- function(...) pretty_message(emoji = "white_question_mark", ...)
+
+
 
 
     
