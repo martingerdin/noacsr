69684fbd063ba4283f5b3c0f91ff3328fe46b546
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-07-28 21:01:08 +0200

    Update util functions

--- R/utils.R
+++ R/utils.R
@@ -4,14 +4,18 @@ pretty_message <- function(...) message (prettify_text(...))
 
 pretty_warning <- function(...) warning (prettify_text(...))
 
-prettify_text <- function(emoji = NULL, ...) {
+prettify_text <- function(..., emoji = NULL) {
     strwrap(paste0(ifelse(is.null(emoji), "", emoji::emoji(emoji)),
                    ifelse(is.null(emoji), "", " "),
                    ...),
-            prefix = "\n")   
+            prefix = "\n", initial = "")   
 }
 
 ask_question <- function(...) pretty_message(emoji = "white_question_mark", ...)
+
+request_information <- function(...) pretty_message(emoji = "white_exclamation_mark", ...)
+
+request_confidential <- function(...) askpass::askpass(prompt = request_information(...))
 
 
 
