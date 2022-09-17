073f38e86e876388b2b69efd6e2938c47f6524c6
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-09-17 23:41:57 +0200

    Draft create_function function

---
+++ R/create_function.R
@@ -0,0 +1,26 @@
+#' Create Function
+#'
+#' Creates a file in the directory `/functions` with some function
+#' boilerplate to facilitate creating functions and write code that is
+#' easier to understand and maintain.
+#' @param title Character. What the function does, for example
+#'     "Create function" of "Create sample characteristics table". No
+#'     default.
+#' @export
+create_function <- function(title) {
+    assertthat::assert_that(is.character(title) & length(title) == 1)
+    roxygen.title <- tools::toTitleCase(title)
+    function.name <- snakecase::to_snake_case(title)
+    boilerplate <- paste0("#' ", roxygen.title, "\n",
+                          "#'\n",
+                          "#' Write a short description of your function here \n",
+                          "#' @param x Describe argument x here \n",
+                          "#' @export \n",
+                          function.name, " <- function(x) {\n",
+                          "    ## Replace with the contents of your function\n",
+                          "    y <- x + 1\n",
+                          "    return (y)\n",
+                          "}")
+    write(boilerplate, paste0("functions/", function.name, ".R"))
+}
+
